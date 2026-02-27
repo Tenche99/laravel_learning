584519d22e454c9afc09e194222be6e91f8c0bc8
Tenzen Cheme <101424540+Tenche99@users.noreply.github.com>
2026-02-27 10:44:42 +0530

    halfway routes reloaded (#4)
    * halfway routes reloaded
    
    * routes tips

--- routes/web.php
+++ routes/web.php
@@ -1,91 +1,9 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Job;
 
+Route::view('/', 'home');
+Route::view('/contact', 'contact');
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
-
-//index
-Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->latest()->simplePaginate(3);
-
-    return view('jobs.index', [
-        'jobs' => $jobs
-    ]);
-});
-
-//create
-Route::get('/jobs/create', function () {
-    return view('jobs.create');
-});
-
-//show
-Route::get('/job/{id}', function ($id) {
-
-    $job = Job::find($id);
-
-    return view('jobs.show', [
-        'job' => $job
-    ]);
-});
-
-//store
-Route::post('/jobs', function () {
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required'],
-    ]);
-    Job::create([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'employer_id' => 1
-    ]);
-    return redirect('/jobs');
-});
-
-//edit
-Route::get('/job/{id}/edit', function ($id) {
-
-    $job = Job::find($id);
-
-    return view('jobs.edit', [
-        'job' => $job
-    ]);
-});
-
-//update
-Route::patch('/job/{id}', function ($id) {
-    //validate
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required']
-    ]);
-    //authorize (on hold)
-    //update
-    $job = Job::findOrFail($id);
-
-    $job->update([
-        'title' => request('title'),
-        'salary' => request('salary')
-    ]);
-    //redirect
-    return redirect('/job/' . $job->id);
-
-});
-
-//detroy
-Route::delete('/job/{id}', function ($id) {
-    $job = Job::findOrFail($id);
-    $job->delete();
-    return redirect('/jobs');
-
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
-
-require __DIR__ . '/settings.php';
+Route::resource('jobs', JobController::class);
