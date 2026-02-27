2d22bb08438a6bf66ad3a971252b9e02476089ff
Tenche99 <chemetenzin99@gmail.com>
2026-02-27 10:43:32 +0530

    routes tips

--- routes/web.php
+++ routes/web.php
@@ -2,39 +2,8 @@
 
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Job;
 
+Route::view('/', 'home');
+Route::view('/contact', 'contact');
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
-
-//index
-Route::get(
-    '/jobs',
-    [JobController::class, 'index']
-);
-
-//create
-Route::get('/jobs/create', [JobController::class, 'create']);
-
-//show
-Route::get('/job/{job}', [JobController::class, 'show']);
-
-//store
-Route::post('/jobs', [JobController::class, 'store']);
-
-//edit
-Route::get('/job/{job}/edit', [JobController::class, 'edit']);
-
-//update
-Route::patch('/job/{job}', [JobController::class, 'update']);
-
-//destroy
-Route::delete('/job/{job}', [JobController::class, 'destroy']);
-
-Route::get('/contact', function () {
-    return view('contact');
-});
-
-require __DIR__ . '/settings.php';
+Route::resource('jobs', JobController::class);
